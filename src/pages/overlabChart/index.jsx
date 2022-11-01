534f5f4858b64aafd6f27b3de60297a28575cb80
ZhangWen <zhangwen1932@hotmail.com>
2022-11-01 20:55:57 +0800

    添加eslint

--- src/pages/overlabChart/index.jsx
+++ src/pages/overlabChart/index.jsx
@@ -1,29 +1,27 @@
-import { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import * as echarts from 'echarts';
 import { timeData, timeData2 } from './data';
 
-
 // 重叠折线图
-export default function BarChart(){
+export default function BarChart() {
   const chartRef = useRef();
   useEffect(() => {
     const testChart = echarts.init(chartRef.current);
     const option = {
       title: {
         text: '测试图表',
-        left: 'center'
+        left: 'center',
       },
       xAxis: [{
         // type: 'time',
-        // data: ['2022-10-1', '2022-10-2', '2022-10-3', '2022-10-4', '2022-10-5', '2022-10-6', '2022-10-7']
-        type: "category",
+        type: 'category',
         data: timeData.map((item) => item.time),
       }],
-       yAxis: [{
-        type: 'value'
+      yAxis: [{
+        type: 'value',
       }],
       tooltip: {
-        trigger: 'axis'
+        trigger: 'axis',
       },
       visualMap: {
         show: false,
@@ -36,25 +34,25 @@ export default function BarChart(){
           data: timeData,
           symbol: 'none',
           lineStyle: {
-            color: "red",
-          }
+            color: 'red',
+          },
         },
         {
           type: 'line',
           data: timeData2,
           symbol: 'none',
           lineStyle: {
-            color: "transparent",
+            color: 'transparent',
           },
           areaStyle: {
-            color: 'green'
-          }
-        }
-      ]
+            color: 'green',
+          },
+        },
+      ],
     };
     testChart.setOption(option);
   }, []);
   return (
-    <div ref={chartRef} style={{ height: '600px', width: '600px' }}/>
-  )
+    <div ref={chartRef} style={{ height: '600px', width: '600px' }} />
+  );
 }
